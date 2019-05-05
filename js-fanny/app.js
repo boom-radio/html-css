76a2f42b84496136d8d56de78e98bae1d9f67c32
MamzelNyf <you@example.comfanny.lefert@gmail.com>
2019-05-05 17:34:46 +0800

    listen with tabs

--- js-fanny/app.js
+++ js-fanny/app.js
@@ -1,3 +1,20 @@
 $(document).foundation()
+//BEGINNING OF JS FOR SCROLLING-STICKY TOP BAR
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
 
+// Get the header
+var header = document.getElementById("topBarContainer");
 
+// Get the offset position of the navbar
+var sticky = topBarContainer.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    topBarContainer.classList.add("sticky-test");
+  } else {
+    topBarContainer.classList.remove("sticky-test");
+  }
+}
+//END OF JS FOR SCROLLING-STICKY TOP BAR
